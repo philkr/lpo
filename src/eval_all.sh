d4068c5e0b98e912a3eb34a45e7181a5dc07b19e
philkr <philkr@users.noreply.github.com>
2014-12-16 12:04:46 -0800

    Version of the code that matches the CVPR 2015 paper

---
+++ src/eval_all.sh
@@ -0,0 +1,7 @@
+# This script reproduces trable 2 in the paper
+python3 train_lpo.py -f0 0.2 ../models/lpo_VOC_0.2.dat
+python3 train_lpo.py -f0 0.1 ../models/lpo_VOC_0.1.dat
+python3 train_lpo.py -f0 0.05 ../models/lpo_VOC_0.05.dat
+python3 train_lpo.py -f0 0.03 ../models/lpo_VOC_0.03.dat
+python3 train_lpo.py -f0 0.02 ../models/lpo_VOC_0.02.dat
+python3 train_lpo.py -f0 0.01 ../models/lpo_VOC_0.01.dat -iou 0.925 # Increase the IoU a bit to make sure the number of proposals match
