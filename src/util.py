3853941a6bb637ee1cbbee418d7be3f25b1df2cf
Rodrigo Benenson <benenson@mpi-inf.mpg.de>
2015-05-13 20:14:39 +0200

    Making python scripts (a bit) more user friendly

--- src/util.py
+++ src/util.py
@@ -25,6 +25,9 @@
 	 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
+
+from __future__ import print_function
+
 from sys import stdout
 
 try:
