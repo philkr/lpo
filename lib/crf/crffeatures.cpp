2835ae022c5804ffad058571ecab45e20a1012bc
philkr <philkr@users.noreply.github.com>
2015-06-04 10:29:48 -0700

    Resolvning a filename conflict in features.cpp

--- lib/crf/crffeatures.cpp
+++ lib/crf/crffeatures.cpp
@@ -24,7 +24,7 @@
 	 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#include "features.h"
+#include "crffeatures.h"
 #include "imgproc/color.h"
 #include "segmentation/segmentation.h"
 #include "util/util.h"
